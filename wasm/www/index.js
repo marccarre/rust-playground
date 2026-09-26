be31910de873d496b5307ade4b8d001a389072cd
Marc Carré <carre.marc@gmail.com>
2026-09-26 21:17:43 +0900

    feat(wasm): add play/pause button

--- wasm/www/index.js
+++ wasm/www/index.js
@@ -67,11 +67,40 @@ const drawCells = () => {
   ctx.stroke();
 };
 
+const playPauseButton = document.getElementById("btn-play-pause");
+
+let animationId = null;
+
+const isPaused = () => {
+  return animationId === null;
+};
+
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  renderLoop();
+};
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  animationId = null;
+};
+
+playPauseButton.addEventListener("click", (event) => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
+
 const renderLoop = () => {
   universe.tick();
   drawGrid();
   drawCells();
-  requestAnimationFrame(renderLoop);
+  animationId = requestAnimationFrame(renderLoop);
 };
 
-requestAnimationFrame(renderLoop);
+playPauseButton.textContent = "▶";
+drawGrid();
+drawCells();
