39d10dc024fd61473f06301bc0420db7bc10d13e
Marc Carré <carre.marc@gmail.com>
2026-09-26 22:10:24 +0900

    fix(wasm): support logging in native tests

--- wasm/src/utils.rs
+++ wasm/src/utils.rs
@@ -1,8 +1,13 @@
+#[cfg(target_arch = "wasm32")]
 extern crate web_sys;
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
-    ( $( $t:tt )* ) => {
+    ( $( $t:tt )* ) => {{
+        #[cfg(target_arch = "wasm32")]
         web_sys::console::log_1(&format!( $( $t )* ).into());
-    }
+
+        #[cfg(not(target_arch = "wasm32"))]
+        eprintln!($( $t )*);
+    }}
 }
